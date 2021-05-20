21ebeaef4ede4723c6fa7d3152c303a2d92815e3
Antoine Assal <antoine.assal@mail.concordia.ca>
2021-05-20 00:50:50 -0400

    Queue Based Breadth First Search

---
+++ 472-Artifical_Intelligence/Scripts/2_2_Queue_Based_BFS.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+def search(start, goal, next_states):
+    Frontier = deque([start])
+    Parent = {start:start}
+    while len(Frontier) > 0 :
+        state = Frontier.popleft()
+        if state == goal:
+            return path_to(state, Parent)
+        new_states = next_states(state)
+        for ns in new_states:
+            if ns not in Parent:
+                Parent[ns] = state
+                Frontier.append(ns)
+                
+def path_to(state, Parent):
+    p = Parent[state]
+    if p == state:
+        return [state]
+    return path_to(p, Parent) + [state]
